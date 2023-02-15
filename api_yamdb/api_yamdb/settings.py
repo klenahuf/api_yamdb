aa88d26fdd3c84fb2ea151958ee009572f6de15e
Olga Komleva <olyakomleva@mail.ru>
2023-02-15 14:46:21 +0300

    Слияние ветки из девелоп

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -130,10 +130,8 @@ SIMPLE_JWT = {
 
 AUTH_USER_MODEL = 'users.User'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
